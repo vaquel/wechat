3a92ba1fe0ac860d34dd9f3ef279295b455f95c0
ChenLong <>
2019-07-09 15:58:58 +0800

    fix

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -10,7 +10,7 @@ class WeChatController extends Controller
     {
         $app = app('wechat.official_account');
         $app->server->push(function ($message) {
-            return "欢迎关注 overtrue！".$message;
+            return "欢迎关注 overtrue！";
         });
         return $app->server->serve();
     }
